b7aafafb0ef1b385eb318dea6696acb39c398562
readel <133009813+readel23@users.noreply.github.com>
2026-02-15 22:02:17 +0500

    Add files via upload
    0.1v

---
+++ products.js
@@ -0,0 +1,92 @@
+const products = [
+    {
+        id: 1,
+        name: "Хлеб формовой",
+        price: 180,
+        unit: "шт",
+        image: "img/bread.jpg",
+        stock: 50,
+        category: "Выпечка"
+    },
+    {
+        id: 2,
+        name: "Масло сливочное 82.5%",
+        price: 950,
+        unit: "шт",
+        image: "img/butter.jpg",
+        stock: 24,
+        category: "Молочные продукты"
+    },
+    {
+        id: 3,
+        name: "Сыр Голландский",
+        price: 3400,
+        unit: "кг",
+        image: "img/cheese.jpg",
+        stock: 15,
+        category: "Молочные продукты"
+    },
+    {
+        id: 4,
+        name: "Шоколад молочный",
+        price: 450,
+        unit: "шт",
+        image: "img/chocolate.jpg",
+        stock: 100,
+        category: "Сладости"
+    },
+    {
+        id: 5,
+        name: "Кофе растворимый",
+        price: 2800,
+        unit: "банка",
+        image: "img/coffee.jpg",
+        stock: 12,
+        category: "Напитки"
+    },
+    {
+        id: 6,
+        name: "Яйца куриные (C1)",
+        price: 550,
+        unit: "дес.",
+        image: "img/eggs.jpg",
+        stock: 300,
+        category: "Бакалея"
+    },
+    {
+        id: 7,
+        name: "Сок мультифрукт 1л",
+        price: 620,
+        unit: "шт",
+        image: "img/juice.jpg",
+        stock: 45,
+        category: "Напитки"
+    },
+    {
+        id: 8,
+        name: "Молоко 2.5%",
+        price: 420,
+        unit: "л",
+        image: "img/milk.jpg",
+        stock: 60,
+        category: "Молочные продукты"
+    },
+    {
+        id: 9,
+        name: "Чай черный листовой",
+        price: 750,
+        unit: "пачка",
+        image: "img/tea.jpg",
+        stock: 80,
+        category: "Напитки"
+    },
+    {
+        id: 10,
+        name: "Йогурт питьевой",
+        price: 280,
+        unit: "шт",
+        image: "img/yogurt.jpg",
+        stock: 35,
+        category: "Молочные продукты"
+    }
+];
