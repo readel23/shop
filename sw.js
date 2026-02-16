f3917d47b272aa54c0657a8cb03500f66cd8dc27
readel <133009813+readel23@users.noreply.github.com>
2026-02-16 19:00:11 +0500

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Этот код позволяет приложению работать быстрее
-  e.respondWith(fetch(e.request));
-});
